a0065716d98ef3953407358db15a1014d119b719
Friedemann Ang <friedemann.ang@gmail.com>
2020-09-29 18:29:22 +0800

    nameprice cleaning logic and write back to s3

--- 1. Data Pipelines/nameprice_daily.py
+++ 1. Data Pipelines/nameprice_daily.py
@@ -11,27 +11,66 @@ from botocore.exceptions import ClientError
 
 import os
 import pandas as pd
+import re
 
 # DAG-specific constants
 SECRET_NAME = 'govtech2020-test'
 REGION_NAME = 'us-east-1'
-BUCKET_NAME = 'nameprice'
-KEY = 'dataset.csv' 
+IN_BUCKET_NAME = 'nameprice'
+IN_KEY = 'dataset.csv' 
+OUT_BUCKET_NAME = 'clean_nameprice'
+OUT_KEY = 'clean_dataset.csv'
+IN_FILEPATH = os.path.join('data', 'dataset.csv')
+OUT_FILEPATH = os.path.join('output', 'result.csv')
 
+# main task callable
 def process_nameprice(**kwargs):
-	
 # retrieve data file
 	s3_client = boto3.client('s3')
 
 	try:
-	    s3_client.download_file(BUCKET_NAME, KEY, 'data/dataset.csv')
+	    s3_client.download_file(IN_BUCKET_NAME, IN_KEY, IN_FILEPATH)
 	except botocore.exceptions.ClientError as e:
 	    if e.response['Error']['Code'] == "404":
 	        print("The object does not exist.")
 	    else:
 	        raise
 
-	return 'End'
+# process file and write to output/result.csv
+	result_summary = clean_nameprice(datafile)
+
+	s3_client.upload_file(OUT_FILEPATH, OUT_BUCKET_NAME, OUT_KEY)
+
+	return result_summary
+
+# data cleaning logic
+def clean_nameprice(datafile):
+	df = pd.read_csv(IN_FILEPATH)
+	in_shape = df.shape
+
+	outdf = df.copy()
+# take last 2 space-delimited tokens as first and last name. assumes name field has at least 2 space-delimited tokens in string
+	outdf['firstname'] = outdf['name'].apply(lambda x: re.split(' ',x)[-2])
+	outdf['lastname'] = outdf['name'].apply(lambda x: re.split(' ',x)[-1])
+# pandas automatically reads numeric column, removing prepended 0s
+# remove rows with no name
+	outdf = outdf.loc[outdf['name'] != '']
+# flag if price > 100
+	outdf['above_100'] = outdf['price'].apply(lambda x: 'true' if x > 100 else 'false')
+
+	outdf = outdf[['firstname', 'lastname', 'price', 'above_100']]
+# write to output file
+	out_df.to_csv(OUT_FILEPATH, index=False)
+
+	outshape = outdf.shape
+
+# log diff in records between infile and outfile
+	result = {
+		'inshape': inshape
+		'outshape': outshape
+	}
+
+	return result
 
 
 # DAG boilerplate
